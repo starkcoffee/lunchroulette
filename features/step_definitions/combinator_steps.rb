47e55c8cfa163418365605c33c8fdec348d7714f
Duana Stanley <duana.stanley@gmail.com>
2012-06-17 17:38:48 +0200

    add route for showing combinators

--- features/step_definitions/combinator_steps.rb
+++ features/step_definitions/combinator_steps.rb
@@ -1,3 +1,8 @@
 Given /^there is no combinator for (.+)$/ do |name|
     Combinator.find_by_name(name).should be_nil
 end
+
+When /^I go to the combinator link for (.+)$/ do |name|
+    visit("/#{name}")
+end
+
