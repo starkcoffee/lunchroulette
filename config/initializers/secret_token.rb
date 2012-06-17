14cf265dbf8cf472477e380f95e50781691ff2ed
Duana Stanley <duana.stanley@gmail.com>
2012-06-17 09:18:59 +0200

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Combinasaurus::Application.config.secret_token = '5392a4ceb69478838c8d826215d31a00e6b3c0a00d88ce30511e5a60f4c4033d040b76b36b800fbf8eecbc26a48edb2eb14ae28a5e2cb9a3eca3499a1e753363'
